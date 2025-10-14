539480085aef1eeb74a6125e7dba3fe3cd04c6f7
Lorenzo <ing.lrmartinez@gmail.com>
2025-10-14 16:44:57 +0900

    implemented a gradio small demo .

---
+++ gradio_demo/gr_app.py
@@ -0,0 +1,35 @@
+import gradio as gr
+
+def fn_shout(text:str) -> str:
+    return text.upper()
+
+#sets dark mode
+force_dark_mode="""
+function refresh(){
+  const url = new URL(window.location);
+  if (url.searchParams.get('__theme') !== 'dark'){
+    url.searchParams.set('__theme','dark');
+    window.location.href = url.href
+  }
+}
+"""
+
+
+gr.Interface(
+    fn=fn_shout,
+    #inputs="textbox",
+    inputs=[gr.Textbox(label="Your message",lines=6)],
+    outputs="textbox",
+    title="Shout Demo", 
+    flagging_mode="never",
+    js=force_dark_mode
+).launch(share=True)
+
+#options in lanch :
+#share=true , create a share url that is public
+#inbrowser=true, opens the app in the browser [works when you are using jupyter]
+#empty, just open the app and show the local url.
+
+#flagging_mode=never, sets the flags dissabled and store the flags in .gradio folder
+
+    
